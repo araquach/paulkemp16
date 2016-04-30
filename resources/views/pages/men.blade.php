cff9d029f76f5cf9343e9a7dab03d24ec8cf6a7d
Adam Carter <adam@jakatasalon.co.uk>
2016-04-30 16:28:47 +0000

    Fixed the form showing the mens image

--- resources/views/pages/men.blade.php
+++ resources/views/pages/men.blade.php
@@ -15,8 +15,8 @@
 
 @section('content')
 
-<section id="men">
-  <section id="meninfo">
+<section id="mens">
+  <section id="mensinfo">
     <h2>Men's Hairdressing</h2>
     <p>At Paul Kemp Hairdressing we have a team of experts in men's hairdressing on hand. Whatever your fashion tastes and style, we'll work your hair exactly how you want it.</p>
     <p>The team constantly stay up to date with latest cutting &amp; styling techniques through Tigi and Schwarzkopf's innovative training courses and inspiration sessions.</p>
@@ -25,7 +25,7 @@
     
     
     </section>
-    <div id="menimages">
+    <div id="mensimages">
     {{ Html::image('images/men/men1.jpg') }}
     {{ Html::image('images/men/men2.jpg') }}
     {{ Html::image('images/men/men3.jpg') }}
