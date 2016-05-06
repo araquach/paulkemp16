0351206507bb99972e7908caf6db7e974d87df36
Adam Carter <adam@jakatasalon.co.uk>
2016-05-06 10:53:18 +0000

    Created offer partials

---
+++ resources/views/layouts/partials/offers/cut_col.blade.php
@@ -0,0 +1,41 @@
+{{-- Colour Packages Offers --}}
+
+<section id="offerlist">
+
+<div id="offer1" class="offer"><h3>Senior Stylist Colour &amp; Cut Package</h3>
+<p>for just &pound;80</p>
+<p>with any of our Senior Stylists</p>
+<p class="small">offer until <time datetime="2015-09-30">30/09/15</time><br>
+  For New Clients Only - not with any other offer - excludes Saturday<br>Surcharge for Dark to lights may apply<br>Skin test required 48hrs prior to service</p>
+</div>
+
+<div id="offer2" class="offer">
+  <h3>Stylist Colour &amp; Cut Package</h3>
+  <p>for just &pound;65</p>
+  <p>with any of our Stylists</p>
+    <p class="small">offer until <time datetime="2015-09-30">30/09/15</time><br>
+      For New Clients Only - not with any other offer - excludes Saturday<br>Surcharge for Dark to lights may apply<br>Skin test required 48hrs prior to service</p>
+</div>
+
+<div id="offer4" class="offer">
+  <h3>Graduate Stylist Colour &amp; Cut Package</h3>
+  <p>for just &pound;50</p>
+  <p>with any of our Graduate Stylists</p>
+    <p class="small">offer until <time datetime="2015-09-30">30/09/15</time><br>
+      For New Clients Only - not with any other offer - excludes Saturday<br>Surcharge for Dark to lights may apply<br>Skin test required 48hrs prior to service</p>
+</div>
+
+
+<div id="offer3" class="offer">
+  <h3>Men's Cut &amp; Style</h3>
+  <p>&pound;16 with a Stylist<br>&pound;20 with a Senior Stylist<br></p>
+    <p class="small">until <time datetime="2015-09-30">30/09/15</time><br>
+      For New Clients Only - not with any other offer - excludes Saturday</p>
+</div>
+
+{{-- FB Like Button --}}
+
+
+</section>
+
+{!! link_to('contact', 'Online Booking &amp; Enquiry form', array('class' => 'book_button')) !!}
--- resources/views/pages/offers.blade.php
+++ resources/views/pages/offers.blade.php
@@ -21,24 +21,9 @@
 
 <h2>Offers</h2>
 
-<section id="offerlist">
+@include('partials.offers.no_offer')
 
-<div id="offer1" class="offer">
-<h3>FREE Colour <br>with every<br>Cut, Dry &amp; Style</h3>
-<p>For new clients<br>until the end of May</p>
-<p class="small">offer until <time datetime="2014-05-30">30/05/14</time><br>
-  For New Clients Only - not with any other offer - excludes Saturday<br>Skin test required 48hrs prior to service</p>
-</div>
-  
-<div id="offer4" class="offer">
-  <h3>FREE Cut, Dry &amp; Style</h3>
-  <p>with every Kebelo System Treatment</p><p class="small">until <time datetime="2014-05-30">30/05/14</time><br>
-    not with any other offer<br>Excludes Saturday</p>
-</div>
- 
-</section> <!--end offerlist-->
-
-{!! link_to('contact', 'Online Booking &amp; Enquiry form', array('class' => 'book_button')) !!}
+@stop
  
 </section> <!--end offersMain-->
 
