0694d4290ac8de6419f161677d47bf82c0649e8c
Adam Carter <adam@jakatasalon.co.uk>
2016-05-10 21:18:10 +0000

    Created Feedback stuff

---
+++ app/FeedbackClient.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FeedbackClient extends Model
+{
+    protected $guarded = ['id']; 
+}
---
+++ app/Http/Requests/FeedbackRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class FeedbackRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'intro' => 'required',
+            'consultation' => 'required',
+            'styling_area' => 'required',
+            'stylist_appearance' => 'required',
+            'prod_advice' => 'required',
+            'styling_advice' => 'required',
+            'mkt1' => 'required',
+            'mkt2' => 'required',
+            'mkt3' => 'required',
+            'value_for_money' => 'required',
+            'whole_experience' => 'required',
+            'end_result' => 'required',
+            'extra' => '',
+            'extra_edit' => '',
+        ];
+    }
+}
