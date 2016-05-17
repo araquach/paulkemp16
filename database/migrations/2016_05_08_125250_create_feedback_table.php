413da9fee1283b4c14e7c399042c7b459fd87da6
Adam Carter <adam@jakatasalon.co.uk>
2016-05-17 17:54:38 +0000

    Added nullable values to Feedback table

--- database/migrations/2016_05_08_125250_create_feedback_table.php
+++ database/migrations/2016_05_08_125250_create_feedback_table.php
@@ -21,17 +21,17 @@ class CreateFeedbackTable extends Migration
             $table->integer('stylist_appearance');
             $table->integer('prod_advice');
             $table->integer('styling_advice');
-            $table->integer('mkt1');
-            $table->integer('mkt2');
-            $table->integer('mkt3');
+            $table->integer('mkt1')->nullable();
+            $table->integer('mkt2')->nullable();
+            $table->integer('mkt3')->nullable();
             $table->integer('value_for_money');
             $table->integer('whole_experience');
             $table->integer('end_result');
-            $table->string('extra');
-            $table->string('extra_edit');
+            $table->string('extra')->nullable();
+            $table->string('extra_edit')->nullable();
             $table->integer('client_id');
-            $table->boolean('allow');
-            $table->boolean('publish');
+            $table->boolean('allow')->nullable();
+            $table->boolean('publish')->nullable();
             $table->timestamps();
         });
     }
