d2b14cafd24a595a5f656088ec2df28375033a19
Adam Carter <adam@jakatasalon.co.uk>
2016-06-03 16:24:46 +0000

    Fixed the mobile stlying for ind feedback feed

--- database/seeds/FeedbackClientsTableSeeder.php
+++ database/seeds/FeedbackClientsTableSeeder.php
@@ -13,7 +13,7 @@ class FeedbackClientsTableSeeder extends Seeder
     {
         DB::table('feedback_clients')->insert([
             'iris_id' => '1',
-            'stylist' => 'Maisie Thomson',
+            'stylist' => 'Izzy Lamb',
             'first_name' => 'Patricia',
             'last_name' => 'Carter',
             'mobile' => '0798567373682',
@@ -21,7 +21,7 @@ class FeedbackClientsTableSeeder extends Seeder
         
         DB::table('feedback_clients')->insert([
             'iris_id' => '2',
-            'stylist' => 'Jimmy Sharpe',
+            'stylist' => 'Jo Mahoney',
             'first_name' => 'Sarah',
             'last_name' => 'Reed',
             'mobile' => '07985667545672',
@@ -29,7 +29,7 @@ class FeedbackClientsTableSeeder extends Seeder
         
         DB::table('feedback_clients')->insert([
             'iris_id' => '3',
-            'stylist' => 'Natalie Doxey',
+            'stylist' => 'Michelle Railton',
             'first_name' => 'Helen',
             'last_name' => 'Jones',
             'mobile' => '079846764673682',
@@ -38,7 +38,7 @@ class FeedbackClientsTableSeeder extends Seeder
         DB::table('feedback_clients')->insert([
             'id' => '1456',
             'iris_id' => '4',
-            'stylist' => 'Laura Hall',
+            'stylist' => 'Michelle Railton',
             'first_name' => 'Ryan',
             'last_name' => 'Peters',
             'mobile' => '0733337373682',
