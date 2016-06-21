a3dd115045f0161ec94bab04e2f9806cc77a0bfa
Adam Carter <adam@jakatasalon.co.uk>
2016-06-21 11:30:11 +0000

    Added offer link to home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -34,10 +34,10 @@
     <p><em>The salon has built up a strong reputation for creating stunning, fashionable looks tailored to suit you, and your lifestyle, so if you're looking for a new <strong class="paraend">hairdressers in Warrington</strong> then look no further!</em></p>
 </section>
 
-<a href="{{ URL::to('news') }}#RickAstley">	
+<a href="{{ URL::to('offers') }}">	
 <aside id="news1">
-<h3 class="newsheader">Leon styles pop legend</h3>
-<p class="newspara">PK were drafted in to Cut and style Rick Astleys hair</p>
+<h3 class="newsheader">Latest PK Summer Offers</h3>
+<p class="newspara">Check out the latest new client offers in the salon</p>
 <p class="newscenter"><strong>Find out more here &gt;</strong></p>
 </aside>
 </a>
