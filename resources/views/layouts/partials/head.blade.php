eacdb911e0f637a08e66bdfa616bde8b5246bced
Adam Carter <adam@jakatasalon.co.uk>
2016-08-26 09:54:17 +0000

    fixed the mobile recruitment styling

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -24,6 +24,8 @@
 	</script>
 
 	<link rel="stylesheet" type="text/css" href="{{ elixir('css/app.css') }}" />
+	<link rel="stylesheet" type="text/css" media="only screen and (max-device-width: 640px), only screen and (min-device-width : 719px) and (max-device-width : 721px) and (-webkit-min-device-pixel-ratio : 2) and (orientation : portrait)" href="{{ elixir('css/mob.css') }}"
+	/>
 	
 	<script type="text/javascript" src="{{ URL::asset('scripts/jquery.js') }}"></script>
 	<script type="text/javascript" src="{{ URL::asset('scripts/mootools.js') }}"></script>
