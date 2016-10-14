4ea7d161ae975bed5468d131102b5d3edd0d08e7
Adam Carter <adam@jakatasalon.co.uk>
2016-10-14 09:07:52 +0000

    Removed opt out checkbox

--- resources/views/offer/_form.blade.php
+++ resources/views/offer/_form.blade.php
@@ -4,11 +4,9 @@
     
     {!! Form::hidden('id') !!}
     
-    <div class="row">
-		{!! Form::label('opt_out', 'If you no longer wish to receive offers check the box then click \'Opt Out\'') !!}
-		{!! Form::checkbox('opt_out') !!}
-		{!! $errors->first('opt_out', '<div class="errorMessage">:message</div>') !!}
-	</div>
+    {!! Form::hidden('opt_out', '1') !!}
+	
+	<p>If you no longer wish to receive text offers just click 'Opt Out'</p>
 	
 	<div class="row buttons">
 	{!! Form::submit('Opt Out') !!}
