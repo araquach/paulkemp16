0c29780446d96dd1d33f4fbb2946d2819e448ef3
Adam Carter <adam@jakatasalon.co.uk>
2016-11-07 22:03:29 +0000

    Added November offers

--- resources/views/pages/offers.blade.php
+++ resources/views/pages/offers.blade.php
@@ -3,10 +3,10 @@
 @section('head')
 
 @include('layouts.partials.head', [
-	// 'description' => 'Summer Cut & Colour Packages - £80 with Senior Stylist, £65 with Stylists for new clients',
+	// 'description' => 'Half Price Colour and Cut for new clients throughout November',
 	'keywords' => 'hair offers, hairdressing offers, salon offers, Paul Kemp Hairdressing offers',
 	'ogtitle' => 'Offers',
-	// 'ogdescription' => 'Cut and colour packages: £80 with Senior Stylist, £65 with Stylists for new clients',
+	// 'ogdescription' => 'Half Price Colour and Cut for new clients throughout November',
 	// 'ogimage' => url('/') . '/images/offer/june16_meta.jpg',
 	'title' => 'Paul Kemp Hairdressing - Offers - Hairdressers in Warrington'
 	])
@@ -19,11 +19,11 @@
 
 <section id="offersMain">
 
-<!--<h2>Offers</h2>-->
+<h2>Offers</h2>
 
-@include('pages.offers.no_offer')
+@include('pages.offers.half_price_col')
 
-<!--<div class="fb-like" data-href="http://www.paulkemphairdressing.com/offers" data-width="250" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>-->
+<div class="fb-like" data-href="http://www.paulkemphairdressing.com/offers" data-width="250" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
 
 </section> <!--end offersMain-->
 
