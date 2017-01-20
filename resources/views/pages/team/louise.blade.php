6431adfa10e389ff823fb0715def9b26ddd3b4e5
Adam Carter <adam@jakatasalon.co.uk>
2017-01-20 17:18:54 +0000

    Added Louise

---
+++ resources/views/pages/team/louise.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.main') 
+
+@section('head')
+
+@include('layouts.partials.head', [
+	'description' => '',
+	'keywords' => '',
+	'ogtitle' => 'Louise Bailey',
+	'ogdescription' => '',
+	'ogimage' => url('/') . '/images/staff/louise_og.jpg',
+	'title' => 'Paul Kemp Hairdressing - Louise'
+	])
+	
+@stop
+
+@section('content')
+
+@include('layouts.partials.fb_like')
+
+<script type="text/javascript" src="{{ URL::asset('scripts/jquery.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('scripts/feedback-feed-ind.js') }}"></script>
+
+<section id="team_ind">
+	<div id="team_jo" class="team_pic"></div>
+	
+	<section id="team_ind_copy">
+	<h2>Louise Bailey</h2>
+	<p></p> 
+	<p></p>
+	<p></p>
+	<p class="team_ind_price">Average Cut &amp; Colour price with Louise : &pound;*</p>
+	
+	<div id="feedback_feed_ind">
+  	<ul>
+  	@foreach($feedbacks as $feed)
+  	    <li>&quot;{{ $feed->feedback->extra_edit }}&quot;<br><span class="client">{{ $feed->first_name }} {{ $feed->last_name }} - hair by {{ $feed->stylist }} </span></li>
+  	@endforeach
+  	</ul>
+	</div> <!--#feedback_feed-->
+	
+	<a href="{{ URL::to('team') }}#jo">Click here to see the full team &gt;</a>
+	
+	<div id="fb-like">
+		<div class="fb-like" data-href="http://www.paulkemphairdressing.com/louise" data-width="250" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+	</div>
+	
+</section> <!--team_ind_copy-->
+<section id="teambottom">
+    <p>*average prices for half-head foils or equivalent plus a Cut Dry and Style - before any discounts</p>
+</section>
+</section> <!--team_ind-->
+
+@stop
