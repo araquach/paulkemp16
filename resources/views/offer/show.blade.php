b0b8b7831945b4afd44a3b3ac3aa9e85e6c60a75
Adam Carter <adam@jakatasalon.co.uk>
2017-01-21 17:28:39 +0000

    Added link to Louise on home page

--- resources/views/offer/show.blade.php
+++ resources/views/offer/show.blade.php
@@ -11,49 +11,41 @@
 @stop
 
 @section('content')
-
-@if($offer->gender == 'F')
-
-<div id="special_offer2">
     
-@elseif($offer->gender == 'M')
-
-<div id="special_offer2_male">
-
-@else()
-
-<div id="special_offer2_generic">
-
-@endif
-
+    <section id="new">
+        
     @if(Session::has('message'))
     <div class="applicationSuccess">
     {{{ Session::get('message') }}}
     </div>
     @endif
-    
-    <div id="special_offer_copy">
-        
-        <h1><strong>An even better offer for {{ $offer->first_name }} {{ $offer->last_name }}</strong></h1>
+	
+	<section id="new_copy">
+	
+	<h2>Hi {{ $offer->first_name }}, we'd like to introduce you to out latest stylist...</h2>
+	<p>Louise Bailey joined us in November and is quickly establishing herself within the PK team.</p>
+	<p>She has over two years experience and since qualifying worked in a busy salon in Grappenhall where she specialised in men's hairdressing, before moving over to join the PK team.</p>
+	<p>She is also highly skilled in women's hairdressing, particularily cutting and styling and is now in the process of some intensive colour training to get her colouring skills up to the high standard of the rest of the team.</p>
+	<p>To ease Louise into the team we've introduced her at Graduate stylist level but she will very quickly be moved up to Stylist level within the next few months. 
+	<strong>Now is a great time to book in with her as you won't find a better price for such a skilled stylist - plus we've launched some special offers so you'd be mad not to book in!</strong></p>
+	<p class="so_med"><strong>Special offers for Louise</strong></p>
+	
+	<p class="so_big"><strong>&pound;35</strong></p>
+	<p class="so_med">Cut &amp; Colour Package</p>
+	<p class="so_big"><strong>&pound;20</strong></p>
+	<p class="so_med">Cut, Dry &amp; Style</p>
+	<p class="so_small">with Louise throughout Jan/Feb</p>
+	
+	@include('offer._form')
+	
+	<p class="specOffer_contact"><strong>Call: 01925 444488</strong> to book</p>
+	<p class="so_small">Please note: a skin test required 48 hours prior to any colour service</p>
+	<p class="so_med">We look forward to seeing you in the salon soon!</p>
+	
 
-        <p>We recently sent you an offer {{ $offer->first_name }}, and because we value your custom so much we'd like to give you an even better one!</p> 
-        
-        <p><strong>{!! getOffer2($offer->stylist_level, $offer->gender) !!}<br>on your next visit</strong></p>
-    
-        <small>weekdays only - not with any other offer - offer not transferable</small>
-        
-        <p>Just quote: <strong>1216{{ $offer->stylist_level }}{{ $offer->gender }}</strong> when booking</p>
-        
-        @include('offer._form')
-        
-        <p>Offer Ends: 9th December 2016</p> 
-        
-        <small>Not with any other offer. Not transferable. Weekdays only</small>
-        
-    </div>
+	</section> <!--#new_copy-->
 
-
-</div>
+</section> <!--end #new-->
 
 
 @stop
