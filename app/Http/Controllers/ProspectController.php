c8f564cb9bfb5430cee7b71f273c5302e7db3a2d
Adam Carter <adam@jakatasalon.co.uk>
2017-03-21 13:40:29 +0000

    Fixed the deployment issue

--- app/Http/Controllers/ProspectController.php
+++ app/Http/Controllers/ProspectController.php
@@ -40,9 +40,7 @@ class ProspectController extends Controller
     
     public function freeproducts()
     {
-       // return view('prospect.freeproducts');
-       
-       return "Hello";
+       return view('prospect.freeproducts');
     }
     
     /**
