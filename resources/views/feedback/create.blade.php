dc6debf2d92ba1126ff74691e757f6a08ac5330f
Adam Carter <adam@jakatasalon.co.uk>
2017-04-22 12:00:07 +0000

    Removed the prize draw bit from feedback

--- resources/views/feedback/create.blade.php
+++ resources/views/feedback/create.blade.php
@@ -26,8 +26,7 @@
 	<p>You recently had your hair done by <strong>{{ $client->stylist }}</strong> and we would love to find out how your experience was in the salon.</p>
 	<p>We strive to offer the best possible service, plus give you a hairstyle that you're 100% happy with!</p>
 	<p>Just answer the following questions and give us your honest feedback.</p>
-	<p><em>You will receive a <strong>&pound;10 product voucher</strong> via text that you can use in the salon at any time <strong>PLUS</strong> you'll be entered into our quarterly prize draw for the chance to win great prizes - <strong>including GHD's
-	!</strong></em></p>
+	<p><em>You will receive a <strong>&pound;10 product voucher</strong> via text that you can use in the salon at any time!</p>
 	<p class="small">The voucher is delivered to your phone via text message. Each voucher has a unique reference number that is married up to your booking on the system. The voucher is not transferrable.<br>
 	Your information will not be shared with anyone and is purely for us to ensure we are offering the best possible service along with looking at ways to improve our offering</p>
 </div> <!--feedback_copy-->
--- resources/views/sms/feedback.blade.php
+++ resources/views/sms/feedback.blade.php
@@ -1,1 +1,1 @@
-Hi {{ $first_name }}, this entitles you to £10 towards any product. Voucher code: {{ $id }} See you in the salon soon!
+Hi {{ $first_name }}, this entitles you to £10 towards any product. Voucher code: {{ $id }} See you in the salon soon! 01925 444488
