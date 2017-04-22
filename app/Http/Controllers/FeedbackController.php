d7a75853d9225f2fb9fbdd061fa9422d42ee5488
Adam Carter <adam@jakatasalon.co.uk>
2017-04-22 09:43:32 +0000

    Playing with the feedback to get it working

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -58,12 +58,12 @@ class FeedbackController extends Controller
 		
 		Mail::send('sms.feedback', $recipient, function($message) use ($recipient)
    		{
-       		$message->from('enquiries@jakatasalon.co.uk');
+       		$message->from('adam@jakatasalon.co.uk');
        		
        		$message->subject('PaulKempHair');
 
-       		// $message->to($recipient['mobile'] . '@smsid.textapp.net');
-       		$message->to('adam@jakatasalon.co.uk');
+       		$message->to($recipient['mobile'] . '@smsid.textapp.net');
+       		// $message->to('adam@jakatasalon.co.uk');
    		});
 	    
 	    return redirect()->back()->with('message', 'Thank you for your feedback, it\'s really appreciated. Your voucher is on it\'s way plus you have been entered into our next prize draw for the chance to win some great prizes. See you in the salon soon!');
