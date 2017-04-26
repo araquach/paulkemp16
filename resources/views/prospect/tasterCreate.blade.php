7aeef39246d666bbdcd359a4d5f22be60338e5b1
Adam Carter <adam@jakatasalon.co.uk>
2017-04-26 14:30:52 +0000

    styled the list

--- resources/views/prospect/tasterCreate.blade.php
+++ resources/views/prospect/tasterCreate.blade.php
@@ -19,7 +19,7 @@
 <h2 class="red">Your route to hair perfection</h2>
 
 <p>The <strong>New Client Taster Package</strong> is <span class="red"> completely FREE</span> 
-plus there will be some special incentives on the day! <span class="red">(including FREE products and special offers)</span></p>
+plus there will be some special incentives on the day! <span class="red"><br>(including FREE products and special offers)</span></p>
 <p><strong>Just fill in the form below and we'll be in touch!</strong></p>
 	
 @if(Session::has('message'))
