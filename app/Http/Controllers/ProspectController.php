eaaace54841c68e7dc68fbf00e1e68f4a350c73c
Adam Carter <adam@jakatasalon.co.uk>
2017-05-11 12:56:24 +0000

    removed auth from ProspectController

--- app/Http/Controllers/ProspectController.php
+++ app/Http/Controllers/ProspectController.php
@@ -15,9 +15,7 @@ class ProspectController extends Controller
 {
     public function __construct(Prospect $prospect)
 	{
-		$this->middleware('auth', ['except' => ['freeproducts', 'create', 'store', 'taster', 'tasterCreate', 'tasterStore']]);
-		
-		$this->prospect = $prospect;
+	    $this->prospect = $prospect;
 	}
     
     /**
