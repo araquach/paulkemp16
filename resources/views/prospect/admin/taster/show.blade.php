70c9f1e9e62b7e3daf329b3ac65f9d7f01dd9818
Adam Carter <adam@jakatasalon.co.uk>
2017-05-12 14:05:08 +0000

    Styled the prospect stuff

--- resources/views/prospect/admin/taster/show.blade.php
+++ resources/views/prospect/admin/taster/show.blade.php
@@ -12,59 +12,65 @@
 
 <div id="admin">
 	
-@if(Session::has('message'))
-    <div class="message">
-        {{{ Session::get('message') }}}
-    </div>
-@endif
-
-<h1>Applicant Name: {{ $prospect->first_name }} {{ $prospect->last_name }}</h1>
-
-<ul>
-    <li><strong>Application date:</strong> {{ $prospect->created_at->format('d/m/Y') }}</li>
-    <li><strong>Email Address:</strong> {{ $prospect->email }}</li>
-    <li><strong>Mobile Number:</strong> {{ $prospect->mobile }}</li>
-</ul>
-
-@if(count($prospect->notes) > 0)
-<h2>Notes:</h2>
-@endif
-<ul>
-    @foreach($prospect->notes as $note)
-     
-        <li class="note">{{ $note->created_at->format('d/m/Y') }} - {{ $note->note }}</li>
-    
-    @endforeach
-</ul>
-
-{!! Form::model($prospect, [
-    'method' => 'PATCH',
-    'action' => ['ProspectController@update', $prospect->id],
-]) !!}
+	@if(Session::has('message'))
+	    <div class="message">
+	        {{{ Session::get('message') }}}
+	    </div>
+	@endif
 	
-	<div class="form">
-		<div class="row">
-			{!! Form::label('contact_status', 'Contact Status') !!}
-			{!! Form::select('contact_status', array(
-				'0' => '--Please Select--',
-				'Left Message' => 'Left Message',
-				'Call back' => 'Need to call back',
-				'Booked' => 'Booked Taster Package',
-				'Not Interested' => 'Not Interested',
-				)) !!}
-			{!! $errors->first('contact_status', '<div class="errorMessage">:message</div>') !!}
-		</div>
+	<div id="admin_copy">
+	
+		<h1>Applicant Name: {{ $prospect->first_name }} {{ $prospect->last_name }}</h1>
 		
-		<div class="row buttons">
-		{!! Form::submit('Submit') !!}
+		<ul class="prospect-data">
+		    <li><strong>Application date:</strong> {{ $prospect->created_at->format('d/m/Y') }}</li>
+		    <li><strong>Email Address:</strong> {{ $prospect->email }}</li>
+		    <li><strong>Mobile Number:</strong> {{ $prospect->mobile }}</li>
+		</ul>
+		
+		@if(count($prospect->notes) > 0)
+		<div class="notes">
+			<h2>Notes:</h2>
+			<ul>
+			    @foreach($prospect->notes as $note)
+			     
+			        <li>{{ $note->created_at->format('d/m/Y') }} - {{ $note->note }}</li>
+			    
+			    @endforeach
+			</ul>
 		</div>
+		@endif
+		
+		{!! Form::model($prospect, [
+		    'method' => 'PATCH',
+		    'action' => ['ProspectController@update', $prospect->id],
+		]) !!}
+			
+			<div class="form">
+				<div class="row">
+					{!! Form::label('contact_status', 'Contact Status') !!}
+					{!! Form::select('contact_status', array(
+						'0' => '--Please Select--',
+						'Left Message' => 'Left Message',
+						'Call back' => 'Need to call back',
+						'Booked' => 'Booked Taster Package',
+						'Not Interested' => 'Not Interested',
+						)) !!}
+					{!! $errors->first('contact_status', '<div class="errorMessage">:message</div>') !!}
+				</div>
+				
+				<div class="row buttons">
+				{!! Form::submit('Submit') !!}
+				</div>
+			</div>
+		
+		{!! Form::close() !!}
+		
+		<a href="/prospect/{{ $prospect->id }}/note" class="link-button">Notes</a><br>
+		
+		{!! link_to('prospect/taster', 'Back to all the applicants', ['class' => 'link-button']) !!}
+	
 	</div>
-
-{!! Form::close() !!}
-
-<a href="/prospect/{{ $prospect->id }}/note" class="link-button">Notes</a><br>
-
-{!! link_to('prospect/taster', 'Back to all the applicants', ['class' => 'link-button']) !!}
 
 </div>
 
