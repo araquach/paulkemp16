9ecea20034903ad1f6caa5470dbfcd1b5faee9ea
Adam Carter <adam@jakatasalon.co.uk>
2017-05-13 09:29:24 +0000

    Added FB pixel

--- resources/views/layouts/partials/pros_head.blade.php
+++ resources/views/layouts/partials/pros_head.blade.php
@@ -22,6 +22,25 @@
   		ga('create', 'UA-2487518-2', 'auto');
   		ga('send', 'pageview');
 	</script>
+	
+	<!-- Facebook Pixel Code -->
+	<script>
+	!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+	n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+	document,'script','https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', '1876118849307800'); // Insert your pixel ID here.
+	fbq('track', 'PageView');
+	</script>
+	<noscript><img height="1" width="1" style="display:none"
+	src="https://www.facebook.com/tr?id=1876118849307800&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- DO NOT MODIFY -->
+	<!-- End Facebook Pixel Code -->
+
+
+	
 	{{-- Adobe Typekit Fonts --}}
 	<script src="https://use.typekit.net/bbx0zuv.js"></script>
 	<script>try{Typekit.load({ async: true });}catch(e){}</script>
